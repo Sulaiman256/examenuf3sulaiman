9adbf26ded4d2bcb4a4c5d32a6f7dfde1e47a82e
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2025-05-28 18:42:06 +0200

    Aplica els estils adients al layout perquè totes les pàgines tinguin les fonts tipogràfiques correctes i el color de fons.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: "--font-inter",
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -18,12 +13,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={inter.variable}>
+      <body className={`${inter.className} bg-black text-white min-h-screen antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
